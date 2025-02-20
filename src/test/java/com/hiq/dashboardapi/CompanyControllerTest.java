1f88faf56e2ddafb7031f42e2b90a631d0df07a6
Anton Eliasson <anton.eliasson@hiq.se>
2025-02-20 13:57:36 +0100

    Added test

---
+++ src/test/java/com/hiq/dashboardapi/CompanyControllerTest.java
@@ -0,0 +1,33 @@
+package com.hiq.dashboardapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(CompanyController.class)
+class CompanyControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testGetHello_Success() throws Exception {
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/companies/{id}", 1L)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").value("HiQ Syd AB"));
+    }
+}
+
+
+
